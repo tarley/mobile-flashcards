7dfc3149c68cd1531af1a0d5c28176c681849453
Tarley Campos <tarley.campos@localiza.com>
2019-04-13 21:59:06 -0300

    feat: Configura as fontes da biblioteca native-base
    Conforme descrito na documentação https://docs.nativebase.io/docs/GetStarted.html

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Root, Container, Content, Spinner, Grid} from 'native-base';
+
+import { Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
+  state = { 
+    loading: true 
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+
+    this.setState({ loading: false });
+  }
+
   render() {
+    if (this.state.loading) {
+      return (
+        <Root>
+          <Container >
+            <Content contentContainerStyle={styles.container}>
+              <Spinner color='blue' />
+            </Content>
+          </Container>
+        </Root>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Root>
+        <Container >
+          <Content contentContainerStyle={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </Content>
+        </Container>
+      </Root>
     );
   }
 }
